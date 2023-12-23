165e6b4f3d503ade99c2b386972ee7bd5f7241c8
Hoseong Ahn <hoseong8115@naver.com>
2023-12-23 21:09:09 +0900

    add router and build file

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+import os
+
+app = FastAPI()
+
+# Serve static files (React app)
+app.mount("/static", StaticFiles(directory="build/static"), name="static")
+
+
+# Catch-all route for non-API routes
+@app.get("/{path:path}", include_in_schema=False)
+async def serve_react_app(path: str):
+    index_file_path = os.path.join("build", "index.html")
+    return FileResponse(index_file_path)
